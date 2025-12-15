a28da42ebff89c5f6aac0dfee9674a49192d7d42
SafatUddin <akmsafat@gmail.com>
2025-12-15 19:40:26 +0600

    Fixed Decorator for decription

--- cars/patterns/decorator.py
+++ cars/patterns/decorator.py
@@ -5,6 +5,10 @@ class CarComponent(ABC):
     @abstractmethod
     def get_price(self):
         pass
+    
+    @abstractmethod
+    def get_description(self):
+        pass
 
 # Concrete Component
 class BasicCar(CarComponent):
@@ -13,6 +17,9 @@ class BasicCar(CarComponent):
     
     def get_price(self):
         return float(self.car.price)
+    
+    def get_description(self):
+        return f"{self.car.year} {self.car.make} {self.car.model}"
 
 # Decorator
 class CarDecorator(CarComponent):
@@ -22,20 +29,36 @@ class CarDecorator(CarComponent):
     @abstractmethod
     def get_price(self):
         pass
+    
+    @abstractmethod
+    def get_description(self):
+        pass
 
 # Concrete Decorators
 class WarrantyDecorator(CarDecorator):
     def get_price(self):
         return self.car_component.get_price() + 50000.00
+    
+    def get_description(self):
+        return self.car_component.get_description() + " + Extended Warranty"
 
 class DashCamDecorator(CarDecorator):
     def get_price(self):
         return self.car_component.get_price() + 15000.00
+    
+    def get_description(self):
+        return self.car_component.get_description() + " + Dash Cam"
 
 class SeatCoversDecorator(CarDecorator):
     def get_price(self):
         return self.car_component.get_price() + 20000.00
+    
+    def get_description(self):
+        return self.car_component.get_description() + " + Custom Seat Covers"
 
 class WindowTintingDecorator(CarDecorator):
     def get_price(self):
         return self.car_component.get_price() + 10000.00
+    
+    def get_description(self):
+        return self.car_component.get_description() + " + Window Tinting"
